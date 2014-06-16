3247dfdf5b25fc75831f00e4a09c0eaa0c8e35fc
Aaron Brown <aaron.kaoru.brown@gmail.com>
2014-06-15 21:04:01 -0700

    Smooth scrolling function added

--- script/main.js
+++ script/main.js
@@ -1,11 +1,26 @@
 
+//hide and show nav bar in mobile mode
 function clickMenu(){
   $("#clickDiv").on("click", function(){
-    //alert("Clicked");
     $("#headerClick").toggleClass("menu");
     $("#headerbar").toggleClass("menuShow");
-    //$("#bumpreturn").toggleClass("bumpheader");
   });
 }
-//alert("loaded");
 clickMenu();
+
+//Smooth scrolling function
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
